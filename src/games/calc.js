ec5ead7aaca1d5d5817def0d9b38b309a48f1cb1
Artem Korytov <artem-korytov@mail.ru>
2023-09-20 23:31:54 +0300

    add new games

---
+++ src/games/calc.js
@@ -0,0 +1,28 @@
+import { getRandom, basicOfGames } from '../index.js';
+
+export default function calcGame () {
+
+    const purposeOfCalcGame = 'What is the result of the expression?';
+
+    const taskForCalcGame = () => {
+
+    const randNum1 = getRandom(100);
+    const randNum2 = getRandom(10);
+    const symbol = ['+', '-', '*'];
+    const randSymbol = symbol[getRandom(2)];
+
+    const question = `${randNum1} ${randSymbol} ${randNum2}`;
+
+    let result;
+    if (randSymbol === symbol[0]) {
+        result = randNum1 + randNum2;
+    } else if (randSymbol === symbol[1]) {
+        result = randNum1 - randNum2;
+    } else {
+        result = randNum1 * randNum2;
+    }
+    result = result.toString();
+    return [question, result];
+};
+basicOfGames(purposeOfCalcGame, taskForCalcGame);
+}
